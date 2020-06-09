f415822ab64ab48e6a5b879cda407ae6c98b1051
Tobias Herber <tobias@herber.space>
2020-06-09 15:30:09 +0200

    Add tests

---
+++ tests/litkey.test.ts
@@ -0,0 +1,87 @@
+import { waitFor, fireEvent } from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
+import addShortcut from '../src';
+
+let callWhenDone = (calls: number, cb: Function) => {
+  let currentCalls = 0;
+
+  return () => {
+    currentCalls++;
+
+    if (currentCalls >= calls) {
+      cb();
+    }
+  }
+};
+
+describe('usage with dom', () => {
+  test('fires event', (cb) => {
+    expect.assertions(1);
+    let div = document.createElement('div');
+    
+    addShortcut('mod+a', () => {
+      expect(true).toBeTruthy();
+      cb();
+    }, div);
+
+    fireEvent.keyDown(div, { key: 'a', ctrlKey: true });
+  });
+
+  test('uses body ad default context', (cb) => {
+    expect.assertions(1);
+    
+    addShortcut('shift+b', () => {
+      expect(true).toBeTruthy();
+      cb();
+    });
+
+    fireEvent.keyDown(document.body, { key: 'b', shiftKey: true });
+  });
+
+  test('accepts array of shortcuts', (cb) => {
+    expect.assertions(3);
+
+    let div = document.createElement('div');
+    let done = callWhenDone(3, cb);
+    
+    addShortcut(['shift+b', 'shift+c', 'mod+9'], () => {
+      expect(true).toBeTruthy();
+      done();
+    }, div);
+
+    fireEvent.keyDown(div, { key: 'b', shiftKey: true });
+    fireEvent.keyDown(div, { key: 'c', shiftKey: true });
+    fireEvent.keyDown(div, { key: '9', ctrlKey: true });
+  });
+
+  test('unregisters events', (cb) => {
+    expect.assertions(1);
+
+    let div = document.createElement('div');
+    
+    let unregister = addShortcut(['shift+b'], () => {
+      expect(true).toBeTruthy();
+      unregister();
+      fireEvent.keyDown(div, { key: 'b', shiftKey: true });
+    }, div);
+
+    fireEvent.keyDown(div, { key: 'b', shiftKey: true });
+
+    setTimeout(cb, 200);
+  });
+
+  test('does not call function if shortcut is not pressed', (cb) => {
+    expect.assertions(1);
+
+    let div = document.createElement('div');
+    
+    addShortcut(['shift+b'], () => {
+      expect(false).toBeTruthy();
+    }, div);
+
+    fireEvent.keyDown(div, { key: '9', ctrlKey: true });
+    expect(true).toBeTruthy();
+
+    setTimeout(cb, 200);
+  });
+});
---
+++ tests/react.test.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { render, waitFor, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { useShortcut } from '../src';
+
+describe('usage with react', () => {
+  test('fires event', async () => {
+    let div = document.createElement('div');
+
+    let Component = () => {
+      let [clicked, setClicked] = useState(false);
+
+      useShortcut('mod+a', () => setClicked(true), [], div);
+
+      return <p>clicked: { '' + clicked }</p>;
+    };
+
+    render(<Component />);
+    await waitFor(() => screen.getByText('clicked: false'));
+
+    fireEvent.keyDown(div, { key: 'a', ctrlKey: true });
+
+    await waitFor(() => screen.getByText('clicked: true'));
+  });
+});
